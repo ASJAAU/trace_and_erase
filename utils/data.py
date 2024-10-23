064bb2a540768d516d01c1f2b47c2dc2a27a90e1
Anders <asjo@create.aau.dk>
2024-10-23 15:30:55 +0200

    Augmentation naming in line with torchvision

--- utils/data.py
+++ utils/data.py
@@ -1,12 +1,16 @@
 from torchvision.transforms import v2 as torch_transforms
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, Dataset
+from torchvision.io import read_image
 import torch
+import pandas as pd
+import os
+import numpy as np
 
 #Data Preprocessing
 def get_transforms(type='train', augments={}):
 
     transform_dict={
-        "horizontal_flip": torch_transforms.RandomHorizontalFlip
+        "RandomHorizontalFlip": torch_transforms.RandomHorizontalFlip
     }
 
     #Define base transforms
@@ -27,10 +31,121 @@ def get_transforms(type='train', augments={}):
     elif type == "test" or type == "valid":
         return torch_transforms.Compose(transforms)
 
-#Dataloaders
-def get_dataset(cfg, split, transforms=None, label_transforms=None):
-    raise NotImplemented
 
-def get_dataloader(cfg, dataset):
-    raise NotImplemented
+class HarborfrontDataset(Dataset):
+    CLASS_LIST = {
+        0: "human",
+        1: "bicycle",
+        2: "motorcycle",
+        3: "vehicle"
+    }
+    def __init__(self, data_split, root, 
+                 transform=None, 
+                 target_transform=None, 
+                 classes=CLASS_LIST.values(), 
+                 binary_labels=False,
+                 classwise=True, 
+                 device='cpu', 
+                 verbose=False) -> None:
+        if verbose:
+            print(f'Loading "{data_split}"')
+            print(f'Target Classes {classes}')
+
+        #Transform objects
+        self.transform = transform
+        self.target_transform = target_transform
+
+        #Use target device for storage
+        self.device = device
+
+        #Load dataset file
+        self.root = root
+        data = pd.read_csv(data_split, sep=";")
+
+        #Isolate desired classes
+        if "all" in classes:
+            print(f"Class: 'all' listed in config. Classes: ]{','.join(self.CLASS_LIST.values())}] will be used.")
+            self.classes = list(self.CLASS_LIST.values())
+        else:
+            for c in classes:
+                assert c in self.CLASS_LIST.values(), f"{c} is not a known class. \n Known classes:{','.join(self.CLASS_LIST.values())}" 
+            self.classes = list(classes)
+
+        #Create dataset of relevant info
+        dataset = {"file_name": list(data['file_name'])}
+        for cls in self.classes:
+            dataset[f'{cls}'] = data[f'{cls}']
+
+        #Reconstruct Dataframe with only training data
+        self.dataset = pd.DataFrame(dataset)
+
+        #Join paths with root
+        self.images = self.dataset.apply(lambda x: os.path.join(root, x["file_name"]), axis=1)
+        
+
+        #Grab labels
+        self.dataset["labels"] = self.dataset.apply(lambda x: np.asarray([int(len(x[g])) for g in self.classes], dtype=np.int8), axis=1)
+
+        #Format labels
+        if binary_labels:
+            self.self.dataset["labels"] = self.dataset.apply(lambda x: np.asarray([1 if g>=1 else 0 for g in x["labels"]], dtype=np.int8), axis=1)
+        if not classwise:
+            self.self.dataset["labels"] = self.dataset.apply(lambda x: np.sum(x["labels"], dtype=np.int8), axis=1)
+        if verbose:
+            print(f'Successfully loaded "{data_split}" as {self.__repr__()}')
+            print("")
+
+    def __len__(self):
+        return len(self.dataset.index)
+
+    def __getitem__(self, idx):
+        image = read_image(self.images.iloc[idx])
+        label = torch.Tensor(self.labels.iloc[idx])
+
+        if self.transform:
+            image = self.transform(image)
+
+        if self.target_transform:
+            label = self.target_transform(label)
+
+        return image.to(self.device), label.to(self.device)
+    
+    def __repr__(self):
+        return self.dataset.__str__()
+
+    def __str__(self):
+        sample=self.__getitem__(0)
+        return f'Harborfront Dataset (Pytorch)' + f"\nExample input: {sample[0].shape} \n{sample[0]}" + f"\nExample label: {sample[1].shape} \n{sample[1]}"
+
+if __name__ == '__main__':
+    from misc import get_config
+    cfg = get_config("configs/base.yaml")
+    subset = "train"
+
+    #Set transforms
+    transforms = get_transforms(subset)
+    label_transforms = get_transforms("label")
+
+    #Load dataset
+    dataset = HarborfrontDataset(
+        data_split=cfg["data"][subset], 
+        root=cfg["data"]["root"], 
+        transform=transforms, 
+        target_transform=transforms, 
+        classes=cfg["data"]["classes"], 
+        binary_labels=cfg["data"]["binary_cls"],
+        classwise=cfg["data"]["classwise"], 
+        device="cpu", 
+        verbose=True)
+
+    #Load dataloader
+    dataloader = DataLoader(
+        dataset,
+        batch_size=cfg["training"]["batch_size"]
+    )
+
+    #Print dummy sample
+    dummy_sample = next(iter(dataloader))
+    print(f"Input Tensor = {dummy_sample[0].shape}")
+    print(f"Label Tensor = {dummy_sample[1].shape}")
 
